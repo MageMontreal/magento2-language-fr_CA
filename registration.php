8987d10cdb77a7f34b53cf165f12a70da78f4ca7
Nicolas MEDINA <nicolas@magemontreal.com>
2021-12-07 13:22:40 -0500

    Package setup + translations

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types=1);
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::LANGUAGE, 'magemontreal_language_fr_ca', __DIR__);
